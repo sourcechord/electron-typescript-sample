8c1ce32ad5abfeb023cb859efe5440d392c7dcfc
minami <sourcechord@gmail.com>
2017-06-10 13:21:21 +0900

    Update packages
    * upgrade electron v1.7.3
    * use electron's official d.ts
    * upgrade typescript v2.3.4

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ const BrowserWindow = remote.BrowserWindow;
 const dialog = remote.dialog;
 
 function hello(){
-	var options: Electron.ShowMessageBoxOptions = {
+	var options: Electron.MessageBoxOptions = {
 		title: 'ダイアログのタイトル',
 		type: 'info',
 		buttons: ['OK', 'Cancel'],
@@ -15,3 +15,5 @@ function hello(){
 	var win = BrowserWindow.getFocusedWindow();
 	dialog.showMessageBox(win, options);
 }
+
+document.getElementById('btnShowHello').onclick = hello;
